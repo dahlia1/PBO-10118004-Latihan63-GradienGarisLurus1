d26daa4ebe25a47eb8de905be1ccb1f7279e7cc3
dahlia1 <55682672+dahlia1@users.noreply.github.com>
2019-10-24 21:04:19 +0700

    Add files via upload

---
+++ PBO-10118004-Latihan63-GradienGarisLurus/src/pbo/pkg10118004/latihan63/gradiengarislurus/GarisLurus.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo.pkg10118004.latihan63.gradiengarislurus;
+
+/**
+ *
+ * @author ASUS A411UF
+ */
+public interface GarisLurus {
+    public int hitungGradien();
+}
